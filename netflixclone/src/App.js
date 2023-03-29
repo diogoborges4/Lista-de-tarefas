6c3e15aabb2976c7f197a9941e2730666f8e3c3b
Diogo Borges <diogoborges9804@gmail.com>
2023-03-28 23:10:22 -0300

    build

--- netflixclone/src/App.js
+++ netflixclone/src/App.js
@@ -1,7 +1,7 @@
 import React, {useEffect, useState} from "react";
 import Tmdb from "./Tmdb";
 import MovieRow from "./components/MovieRow";
-import App from "./App.css"
+import "./App.css"
 import FeatureMovie from "./components/FeatureMovie";
 import Header from "./components/Header";
 
